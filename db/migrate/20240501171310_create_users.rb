f67f6dc37458651bac15cc413052f1639275162f
Devpe007 <anddev007pe@gmail.com>
2024-05-01 14:51:36 -0300

    feature: implementation for user creation

---
+++ db/migrate/20240501171310_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :name, limit: 50, null: false
+      t.string :email, limit: 100, index: { unique: true }, null: false
+      t.string :phone, index: { unique: true }
+      t.string :passaword, limit: 100, null: false
+      t.date :born_at
+
+      t.timestamps null: false
+    end
+  end
+end
